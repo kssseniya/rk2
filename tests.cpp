3d6e56d98da1ba5477cec069c2f6b19bf41ca010
kssseniya <kseniyag2005@gmail.com>
2024-05-19 23:15:03 +0300

    added tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include "text_restricter.cpp" // Include the original implementation file
+
+TEST(TextRestricterTests, NoRestrictionStrategyTest) {
+    TextRestricter textRestricter(std::make_unique<NoRestrictionStrategy>());
+    std::string concatedText;
+    EXPECT_TRUE(textRestricter.ConcatText("hello", " world", concatedText));
+    EXPECT_EQ(concatedText, "hello world");
+}
+
+TEST(TextRestricterTests, CurseRestrictionStrategyTest) {
+    TextRestricter textRestricter(std::make_unique<CurseRestrictionStrategy>());
+    std::string concatedText;
+    EXPECT_FALSE(textRestricter.ConcatText("hello", " fuck", concatedText));
+    EXPECT_TRUE(textRestricter.ConcatText("hello", " world", concatedText));
+    EXPECT_EQ(concatedText, "hello world");
+}
+
+TEST(TextRestricterTests, LengthRestrictionStrategyTest) {
+    TextRestricter textRestricter(std::make_unique<LengthRestrictionStrategy>(5, 10));
+    std::string concatedText;
+    EXPECT_FALSE(textRestricter.ConcatText("short", "text", concatedText));
+    EXPECT_TRUE(textRestricter.ConcatText("good", " text", concatedText));
+    EXPECT_EQ(concatedText, "good text");
+    EXPECT_FALSE(textRestricter.ConcatText("thisis", "toolong", concatedText));
+}
